1ab166831b22b033a0f0b5a3e946d87a718ecee2
Nascimento Davi <davirnascimentox@gmail.com>
2023-12-29 11:27:07 -0300

    first file

---
+++ app.js
@@ -0,0 +1,24 @@
+require ('dotenv').config();
+
+const express = require('express');
+const expressLayout = require('express-ejs-layouts');
+
+const app = express();
+const PORT = 5000 || process.env.PORT;
+
+// Public folder to hold js code
+app.use(express.static('public'));
+
+// Templating engine -> Instead of using html to display data, we use view engines
+// EJS -> Similar sintax to HTML
+app.use(expressLayout);
+app.set('layout', './layout/main');
+app.set('view engine', 'ejs');
+
+app.get('', (req, res) => {
+    res.send("Hello, World!");
+});
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
+});
