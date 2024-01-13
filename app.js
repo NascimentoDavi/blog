488e3b5454aa7f5bc47bb6d358a553f7674ea733
Nascimento Davi <davirnascimentox@gmail.com>
2024-01-13 12:49:15 -0300

    connecting to db

--- app.js
+++ app.js
@@ -3,8 +3,13 @@ require ('dotenv').config();
 const express = require('express');
 const expressLayout = require('express-ejs-layouts');
 
+const connectDB = require('./server/config/db.js');
+
 const app = express();
 const PORT = 5000 || process.env.PORT;
+
+// Connect to DB
+connectDB();
 
 // Public folder to hold js code
 app.use(express.static('public'));
